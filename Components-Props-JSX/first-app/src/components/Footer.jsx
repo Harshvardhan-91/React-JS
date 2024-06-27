2f6a06b5533ad47162a4dd2d45dd39cc556d2954
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-28 00:16:38 +0530

    added

---
+++ Components-Props-JSX/first-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const footer = () => {
+  return (
+    <div className="footer">
+      Copyright &copy; codeWithharry.com | All rights reserved
+    </div>
+  );
+};
+
+export default footer;
