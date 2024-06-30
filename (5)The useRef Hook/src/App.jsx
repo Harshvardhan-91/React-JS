0cda3f9047a4557377e9742e7d5df42c711a2089
Harshvardhan-91 <agarwalharshvardhan081@gmail.com>
2024-06-30 15:16:16 +0530

    added

--- (5)The useRef Hook/src/App.jsx
+++ (5)The useRef Hook/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    alert("Hey welcome to my page");
-  }, []);
-
-  useEffect(() => {
-    alert("count was changed ");
-  }, [count]);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -37,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
